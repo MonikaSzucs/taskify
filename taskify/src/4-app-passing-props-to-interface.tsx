ef4da5704c95ea5ff0054a66afff7d7f7a444c9e
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-04-01 10:05:47 -0700

    32:00 Todo App
    https://www.youtube.com/watch?v=knqz3_rPcKk
    
    Started working on it. Only half way through

--- taskify/src/4-app-passing-props-to-interface.tsx
+++ taskify/src/4-app-passing-props-to-interface.tsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
-import { Todo } from "./model";
 
 // FC is a functional component
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string | number>("");
-  // use [] to state that it will be an array
-  const [todos, setTodos] = useState<Todo[]>([]);
 
   console.log(todo)
 
